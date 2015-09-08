959b375d3d6d8e45f5701d4dbbb3218fc4f02bc0
ep_fl <socialinfo.my@gmail.com>
2015-09-08 09:41:49 +0300

    launch in at simulator

--- WatchApp/AppDelegate.m
+++ WatchApp/AppDelegate.m
@@ -7,16 +7,22 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
-
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    UIWindow *_window;
+}
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    _window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    _window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    [_window makeKeyAndVisible];
+    
     return YES;
 }
 
--- WatchApp/ViewController.m
+++ WatchApp/ViewController.m
@@ -14,14 +14,10 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)loadView {
+    [super loadView];
+    self.title = @"App Watch";
+    self.view.backgroundColor = [UIColor lightGrayColor];
 }
 
 @end
